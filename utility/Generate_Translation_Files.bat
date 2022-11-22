3b0f6b3ddfd963d31ad025a0ec2dc5d7bbfe752a
Yakup Cengiz <yakupcengiz@gmail.com>
2022-11-22 17:20:20 +0300

    Added some utility scripts can be used to generate translation files.

---
+++ utility/Generate_Translation_Files.bat
@@ -0,0 +1,6 @@
+SET QTDIR=C:\Qt\Qt5.10.0\5.10.0\msvc2017_64
+SET PATH=%QTDIR%\bin;%PATH%
+
+REM USAGE :
+REM call UpdateAndReleaseTranslationFromSrc.bat <path> <translartion_file_name>_<language_code>.ts
+
---
+++ utility/UpdateAndReleaseTranslationFromSrc.bat
@@ -0,0 +1,2 @@
+lupdate -recursive %~1 -ts %~2
+lrelease %~2
